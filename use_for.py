b73071eeedcc2ea3396e712d74c3112d131cede3
Kevin W Matthews <KevinWMatthews@gmail.com>
2019-06-11 10:59:30 -0700

    Add example of using 'for' keywork

---
+++ use_for.py
@@ -0,0 +1,30 @@
+class Iterable:
+    def __init__(self, collection):
+        self.collection = collection
+
+    def __iter__(self):
+        # Create the correct iterator
+        return Iterator(self.collection)
+
+class Iterator:
+    def __init__(self, collection):
+        self.collection = collection
+        self.index = 0
+        self.max_index = len(collection)
+
+    def __next__(self):
+        if self.index >= self.max_index:
+            raise StopIteration
+
+        item = collection[self.index]
+        self.index += 1
+        return item
+
+# user creates collection
+collection = (4, 5, 6)
+iterable = Iterable(collection)
+
+# loop over collection
+for item in iterable:
+    # user adds code here
+    print(item)
